035126ca8aafca51dc995dac4e55d5b363240670
Rajandeep20 <Rajandeeplittran@gmail.com>
2025-10-30 19:15:38 -0500

    initialized project

---
+++ src/contants/httpConstants.ts
@@ -0,0 +1,17 @@
+export const HTTP_STATUS = {
+    // Success responses
+    OK: 200,
+    CREATED: 201,
+    NO_CONTENT: 204,
+
+    // Client error responses
+    BAD_REQUEST: 400,
+    UNAUTHORIZED: 401,
+    FORBIDDEN: 403,
+    NOT_FOUND: 404,
+    CONFLICT: 409,
+    UNPROCESSABLE_ENTITY: 422,
+
+    // Server error responses
+    INTERNAL_SERVER_ERROR: 500,
+} as const;
